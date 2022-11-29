2950e8b4d8cb0c0a8458aeb389c56626a2ab13ad
Marcelo Perez <marcelo-perezb@unilibre.edu.co>
2022-11-29 18:16:02 -0500

    AVeryBigSum

---
+++ basic/AVeryBigSum.cs
@@ -0,0 +1,35 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long aVeryBigSum(List<long> ar)
+    {
+        long bigSum = 0;
+        foreach(long num in ar)
+        {
+            bigSum += num;
+        }
+        return bigSum;
+    }
+
+}

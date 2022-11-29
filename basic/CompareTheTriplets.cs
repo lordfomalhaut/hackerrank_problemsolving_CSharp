61366dd8de583dff88f8d61e97f4ccd5ab9fcfb6
Marcelo Pérez Boada <marcelo-perezb@unilibre.edu.co>
2022-11-29 16:43:30 -0500

    Update CompareTheTriplets.cs

--- basic/CompareTheTriplets.cs
+++ basic/CompareTheTriplets.cs
@@ -39,22 +39,3 @@ class Result
     }
 
 }
-
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
-
-        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
-
-        List<int> result = Result.compareTriplets(a, b);
-
-        textWriter.WriteLine(String.Join(" ", result));
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}

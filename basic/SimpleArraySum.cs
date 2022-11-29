9944fdb8c3dc85d4879c4224ef630f2381a310d5
Marcelo Pérez Boada <marcelo-perezb@unilibre.edu.co>
2022-11-29 16:42:23 -0500

    Update SimpleArraySum.cs
    solo mi codigo

--- basic/SimpleArraySum.cs
+++ basic/SimpleArraySum.cs
@@ -34,22 +34,3 @@ class Result
     }
 
 }
-
-class Solution
-{
-    public static void Main(string[] args)
-    {
-        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
-
-        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
-
-        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
-
-        int result = Result.simpleArraySum(ar);
-
-        textWriter.WriteLine(result);
-
-        textWriter.Flush();
-        textWriter.Close();
-    }
-}
